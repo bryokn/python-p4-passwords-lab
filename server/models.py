3cdd2fbf30e59b1a524225ba163dfb18e3f62b11
bryokn <bryokn@gmail.com>
2024-04-13 01:19:49 +0300

    done and test passed

--- server/models.py
+++ server/models.py
@@ -16,13 +16,10 @@ class User(db.Model, SerializerMixin):
 
     @password_hash.setter
     def password_hash(self, password):
-        password_hash = bcrypt.generate_password_hash(
-            password.encode('utf-8'))
-        self._password_hash = password_hash.decode('utf-8')
+        self._password_hash = bcrypt.generate_password_hash(password.encode('utf-8')).decode('utf-8')
 
     def authenticate(self, password):
-        return bcrypt.check_password_hash(
-            self._password_hash, password.encode('utf-8'))
+        return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
 
     def __repr__(self):
         return f'User {self.username}, ID: {self.id}'
